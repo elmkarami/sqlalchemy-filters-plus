097a5382023a6d58b3cc6ae091aab97a2bc009e7
El Mehdi <karami.elmehdi@gmail.com>
2021-03-28 01:24:48 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import pathlib
 from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__)
 
 version = "0.0.1"
 
@@ -7,11 +10,20 @@ setup(
     name="sqlalchemy-filters",
     version=version,
     description="""SQLAlchemy filters made easy""",
+    long_description=(HERE / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     author="El Mehdi Karami",
     author_email="me@elmkarami.com",
-    url="https://github.com/elmkarami/sqlalchemy-filters",
+    url="https://github.com/elmkarami/sqlalchemy-filters-plus",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     zip_safe=False,
     keywords="sqlalchemy,filter,flask,python,sql,query",
+    licence="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ]
 )
